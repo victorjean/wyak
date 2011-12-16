1816ebf3c91d5e36fbcd6b276278bb4c88f51ed7
Victor Jean <victor.jean@gmail.com>
2011-12-15 23:43:00 -0500

    First Project Shell Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wyak::Application.config.secret_token = 'cf06ced098a3fb399f44072599d21eb86be826e8ede0256f5c6c3dda101c3683a248a2be2e6b2917f4797a6de19d6d219022687ba67dd7d0235908bbcc637076'
