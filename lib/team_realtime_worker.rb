80f587633e48d79252210dbb499c1fc9945a54f5
Victor Jean <victor.jean@gmail.com>
2012-04-24 15:56:34 -0400

    Modified Gem Merge for Iron Worker

--- lib/team_realtime_worker.rb
+++ lib/team_realtime_worker.rb
@@ -3,21 +3,18 @@ require 'iron_worker'
 
 class TeamRealtimeWorker < IronWorker::Base
   
-  merge "fantasy_team_helper.rb"
-  merge "real_time_helper.rb"
-  merge "../app/models/team.rb"
-  merge "../app/models/log.rb"
-  merge "../app/models/auth_info.rb"
-  merge "../app/models/player_realtime.rb"
-  merge "../app/models/player_stats.rb"
-  merge "../app/models/player.rb"
-  merge "../app/models/roster.rb"
-  merge "../app/models/user_info.rb"
-  
-  unmerge_gem 'nokogiri'
+  merge_gem 'mongo'
   merge_gem 'mongo_mapper'
   merge_gem 'hpricot'
   merge_gem 'mechanize'
+  unmerge_gem 'nokogiri'
+  
+  merge_folder "../app/models/"
+  
+  merge "fantasy_team_helper.rb"
+  merge "real_time_helper.rb"
+  
+  
   
   
   attr_accessor :team_list
